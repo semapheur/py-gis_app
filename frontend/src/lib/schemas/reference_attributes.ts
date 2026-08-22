dd771b08df70a900472c3f4f185850ff6e28f62b
semapheur <danfyll@hotmail.com>
2026-08-23 00:16:25 +0200

    Backup

---
+++ frontend/src/lib/schemas/reference_attributes.ts
@@ -0,0 +1,42 @@
+export const referenceSchema = {
+  originator: {
+    columns: [
+      {
+        value: "name",
+        label: "Name",
+      },
+      {
+        value: "name_short",
+        label: "Name (short)",
+      },
+      {
+        value: "country",
+        label: "Country",
+      },
+    ],
+  },
+  discipline: {
+    columns: [
+      {
+        value: "name",
+        label: "Name",
+      },
+      {
+        value: "name_short",
+        label: "Name (short)",
+      },
+    ],
+  },
+  source_type: {
+    columns: [
+      {
+        value: "name",
+        label: "Name",
+      },
+      {
+        value: "name_short",
+        label: "Name (short)",
+      },
+    ],
+  },
+};
