6cf103d22ac973f3f98500a8816e8108eba443f6
semapheur <danfyll@hotmail.com>
2026-08-29 09:14:32 +0200

    Backup

--- frontend/src/lib/components/PersonnelForm.svelte
+++ frontend/src/lib/components/PersonnelForm.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
   import Input from "$lib/components/Input.svelte";
   import Select from "$lib/components/Select.svelte";
+  import { getEquipmentOptions } from "$lib/contexts/common.svelte";
+  import type {
+    PersonnelData,
+    PersonnelPointData,
+  } from "$lib/schemas/personnel_annotation";
 
   interface Props {
+    value: PersonnelData;
+    onchange: (value: PersonnelData) => void;
     geometry: "Point" | "Polygon";
   }
 
-  let { geometry }: Props = $props();
+  let { value, geometry }: Props = $props();
+
+  const options = getEquipmentOptions();
 </script>
 
 <form class="personnel-annotation">
   {#if geometry === "Point"}
-    <Select options={[]} placeholder="Confidence" />
+    <Select
+      value={(value as PersonnelPointData).confidence?.id ?? null}
+      options={options.confidence}
+      placeholder="Confidence"
+    />
   {:else if geometry === "Polygon"}
     <Input placeholder="Min count" type="number" min="0" step="1" />
     <Input placeholder="Max count" type="number" min="0" step="1" />
   {/if}
-  <Select options={[]} placeholder="Affiliation" />
+  <Select
+    value={(value as PersonnelPointData).affiliation?.id ?? null}
+    options={options.affiliation}
+    placeholder="Affiliation"
+  />
 </form>
 
 <style>
